6f994d0a5ec4b8419eddacae18f7304b0db9cc0a
amandaoliveira8 <amandaoliveira62819@gmail.com>
2026-03-10 14:05:42 -0300

    Add files via upload

---
+++ questões pratica.js 09.03/main.js
@@ -0,0 +1,4 @@
+var numero1 = 34;
+var numero2 = 8;
+var media = (numero1 + numero2) / 2;
+console.log(" a media é " + media);
---
+++ questões pratica.js 09.03/main02.js
@@ -0,0 +1,8 @@
+let a = parseFloat(prompt("digite lado a: "));
+let b = parseFloat(prompt("digite lado b: "));
+let c = parseFloat(prompt("digite lado c: "));
+let h = parseFloat(prompt("digite lado h: "));
+let perimetro = a + b + c;
+let area = (perimetro*h)/2;
+console.log("o perímetro do triângulo é: " + perimetro);
+console.log("a área do triângulo é: " + area);
